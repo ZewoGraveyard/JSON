4be35ad1d6b449c6281e1035bed61e9b794cf08d
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-19 17:39:33 -0300

    bump to 0.5.0

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "JSON",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/InterchangeData.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/Zewo/StructuredData.git", majorVersion: 0, minor: 5)
     ]
 )
