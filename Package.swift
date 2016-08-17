6b7a117a2341b097a76147651c75271ca579b13f
✪ vi4m Marcin Kliks <vi4m@users.noreply.github.com>
2016-08-17 11:25:36 +0200

    Fix Package open-swift/C7 -> open-swift/C7.git (#19)
    Avoid package errrors with fatal: destination path already exists 
    and is not an empty directory.

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "JSON",
     dependencies: [
-        .Package(url: "https://github.com/open-swift/C7", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/open-swift/C7.git", majorVersion: 0, minor: 12),
     ]
 )
